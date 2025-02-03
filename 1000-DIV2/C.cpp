57e487e74abc8a1203f2b75afc3a44c40ef5e776
IsmailAtchoo <ismail.hatti10@gmail.com>
2025-02-03 18:54:44 +0100

    starting 1000 div2

---
+++ 1000-DIV2/C.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+ 
+#define F first
+#define S second
+#define pb push_back
+#define pf push_front
+#define popf pop_front
+#define popb pop_back
+#define MOD 1000000007
+#define MOD2 998244353
+#define vi vector<int>
+#define vii vector<pair<int,int>>
+#define pi pair<int,int>
+ 
+using namespace std;
+#define ll long long
+int const MAX5 = 100000, MAX6 = 1000000;
+/*
+ 
+*/
+//printf("%.10lf\n",ans);
+//cout<<fixed<<setprecision(20)<<ans<<endl;
+//stoll string -> long long
+
+
+void Solution(){
+    int n; cin >> n;
+    vector<int> tree[n + 1];
+    
+    for(int i = 0; i < n-1; i++){
+        int u, v; cin >> u >> v;
+        tree[u].pb(v);
+        tree[u].pb(u);
+    }
+
+    
+}
+
+int main(){
+  // in & out files
+  /*
+  freopen("outofplace.in", "r", stdin);
+  freopen("outofplace.out", "w", stdout);
+*/
+  // fast and furious io
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  cout.tie(0);
+
+// testcases
+  int t = 1; cin >> t; 
+  while(t--)
+    Solution();
+  return 0;
+
+}
