3cdf5989f22cf151b8324ba02a4588e546d70dc0
IsmailAtchoo <ismail.hatti10@gmail.com>
2025-04-19 13:08:16 +0200

    monsters : unfinished solution

--- CSES/Graph-algorithms/monsters.cpp
+++ CSES/Graph-algorithms/monsters.cpp
@@ -26,6 +26,10 @@ int const MAX5 = 100000, MAX6 = 1000000;
 int vis[1001][1001];
 int n, m;
  
+bool isMonster(int x, int y, string grid[]){
+  return grid[x][y] == 'M';
+}
+
 bool isFloor(int x, int y, string grid[]){
   return grid[x][y] == '.';
 }
@@ -36,12 +40,13 @@ void solve(){
   for(int i = 0; i < n; i++)
     cin >> grid[i];
  
-  int d1[n][m];
-  memset(d1, -1, sizeof(d1));
+  int d[n][m];
+  memset(d, -1, sizeof(d));
   char pred[n][m];
   queue<pair<int, int>> q;
   vector<pair<int,int>> monsters;
   pair<int, int> start, end;
+
   for(int i = 0; i < n; i++){
     for(int j = 0; j < m; j++){
       if(grid[i][j] == 'A'){
@@ -60,8 +65,7 @@ void solve(){
   pair<int, int> moves[4] = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
   char ch_moves[4] = {'R', 'D', 'U', 'L'};
   int x, y;
-  int min_d_to_border = 1000000;
-  int met_monsters = 0;
+  int monsters_in_q = 0;
   while(!q.empty()){
     pair<int, int> curr = q.front();
     x = curr.F;
@@ -71,44 +75,69 @@ void solve(){
       int dx = moves[i].F;
       int dy = moves[i].S;
       if(x + dx >= 0 && x + dx < n && y + dy >= 0 && y + dy < m && (isFloor(x + dx, y + dy, grid) || grid[x+dx][y+dy] == 'M') && !vis[x+dx][y+dy]){
-        q.push({x+dx, y+dy});
+        if(!isMonster(x+dx, y+dy, grid))
+          q.push({x+dx, y+dy});
         vis[x+dx][y+dy] = 1;
         d[x+dx][y+dy] = d[x][y] + 1;
-        if(x+dx == n-1 or y+dy == m-1 or x+dx == 0 or y+dy == 0)
+        //if(x+dx == n-1 or y+dy == m-1 or x+dx == 0 or y+dy == 0)
         pred[x+dx][y+dy] = ch_moves[i];
-        if(grid[x+dx][y+dy] == 'M') met_monsters++;
-        if(met_monsters == cnt_monsters) cout << 
       }
     }
   }
+
+  bool good = false;
   int ans;
-  if(d1[end.F][end.S] == -1){
-    cout << "NO" << endl;
-  }
-  else{
-    ans = d[end.F][end.S];
-    vector<char> path;
-    int currx = end.F, curry = end.S;
-    for(int i = 0; i < ans; i++){
-      path.pb(pred[currx][curry]);
-      if(pred[currx][curry] == 'L'){
-        curry++;
-      }
-      else if(pred[currx][curry] == 'R'){
-        curry--;
-      }
-      else if(pred[currx][curry] == 'U'){
-        currx++;
-      }
-      else{
-        currx--;
-      }
+  for(int i = 0; i < n && !good; i++){
+    if(d[i][0] != -1){
+      end = {i, 0};
+      ans = d[i][0];
+      good = true;
+    } 
+    if(d[i][m-1] !=-1){
+      end = {i, m-1};
+      ans = d[i][m-1];
+      good = true;
     }
-    cout << "YES" << endl;
-    cout << ans << endl;
-    for(int i = ans-1; i >= 0; i--) cout << path[i];
-    cout << endl; 
   }
+  for(int j = 0; j < m && !good; j++){
+    if(d[0][j] != -1){
+      end = {0, j};
+      ans = d[0][j];
+      good = true;
+    }
+    if(d[n-1][j] != -1){
+      end = {n-1, j};
+      ans = d[n-1][j];
+      good = true;
+    }
+  }
+  if(!good){
+    cout << "NO" << endl;
+    return;
+  }
+
+  vector<char> path;
+  int currx = end.F, curry = end.S;
+  for(int i = 0; i < ans; i++){
+    path.pb(pred[currx][curry]);
+    if(pred[currx][curry] == 'L'){
+      curry++;
+    }
+    else if(pred[currx][curry] == 'R'){
+      curry--;
+    }
+    else if(pred[currx][curry] == 'U'){
+      currx++;
+    }
+    else{
+      currx--;
+    }
+  } 
+  cout << "YES" << endl;
+  cout << ans << endl;
+  // cout << path.size() << endl;
+  for(int i = ans-1; i >= 0; i--) cout << path[i];
+  cout << endl; 
 }
  
 int main(){
