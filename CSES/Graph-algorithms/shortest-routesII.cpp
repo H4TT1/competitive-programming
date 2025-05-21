409e13c71f807767e22dcd1e3eede2305fafdcf7
IsmailAtchoo <ismail.hatti10@gmail.com>
2025-05-21 12:40:42 +0200

    short. routes II final solution

--- CSES/Graph-algorithms/shortest-routesII.cpp
+++ CSES/Graph-algorithms/shortest-routesII.cpp
@@ -18,87 +18,40 @@ int const MAX5 = 100000, MAX6 = 1000000;
  
 //printf("%.10lf\n",ans);
 //cout<<fixed<<setprecision(20)<<ans<<endl;
-//stoll string -> long long 
+//stoll string -> long long
+ 
+ 
 /*
 */
 
 const ll INF = 10000000000000;
-vector<vector<pair<int, int>>> adj(100001);
-
-struct myInt{
-  ll i = -1;
-};
-
-map<pair<int,int>, myInt> ans;
-int n;
-
-
-
-
-bool spfa(int s, vector<ll>& d) {
-    d.assign(n + 1, INF);
-    // vector<int> cnt(n + 1, 0);
-    vector<bool> inqueue(n + 1, false);
-    queue<int> q;
-
-    d[s] = 0;
-    q.push(s);
-    inqueue[s] = true;
-    while (!q.empty()) {
-        int v = q.front();
-        q.pop();
-        inqueue[v] = false;
-
-        for (auto edge : adj[v]) {
-            int to = edge.first;
-            ll len = edge.second;
-
-            if (d[v] + len < d[to]) {
-                // cout << d[v] + len << " lmml" << d[to] << endl; 
-                d[to] = d[v] + len;
-                ans[{s, to}].i = d[to];
-                if (!inqueue[to]) {
-                    q.push(to);
-                    inqueue[to] = true;
-                    //cnt[to]++;
-                    //if (cnt[to] > n)
-                        //return false;  // negative cycle (in our case never happens!)
-                }
-            }
-        }
-    }
-    return true;
-}
-
-ll query(int a, int b){
-  vector<ll> d;
-  ll answer = -1;
-  if(ans[{a, b}].i != -1) answer = (answer == -1 ? ans[{a, b}].i : min(answer, ans[{a, b}].i));
-  if(ans[{b, a}].i != -1) answer = (answer == -1 ? ans[{b, a}].i : min(answer, ans[{b, a}].i));
-  if(answer != -1){
-    assert(answer >= 0);
-    //cout << "returned " << answer << endl;
-    //cout << answer << endl;
-    return answer;
-  }
-  spfa(a, d);
-
-  assert((ans[{a, b}].i) >= -1);
-
-  return ans[{a, b}].i;
-}
 
 void solve(){
-  int m, q; cin >> n >> m >> q;
+  int n, m, q; cin >> n >> m >> q;
+  ll dist[n+1][n+1];
+  for(int i = 1; i <= n; i++){
+    for(int j = 1; j <= n; j++){
+      dist[i][j] = ((i == j) ? 0 : INF);
+    }
+  }
+
   for(int i = 0; i < m; i++){
-    int a, b, c; cin >> a >> b >> c;
-    adj[a].pb({b, c});
-    adj[b].pb({a, c});
+    ll a, b, c; cin >> a >> b >> c;
+    dist[a][b] = min(dist[a][b], c);
+    dist[b][a] = dist[a][b];
+  }
+
+  // floyd-warshall
+  for(int k = 1; k <= n; k++)
+    for(int i = 1; i <= n; i++){
+      for(int j = 1; j <= n; j++){
+        dist[i][j] = min(dist[i][k] + dist[k][j], dist[i][j]);
+    }
   }
 
   for(int i = 0; i < q; i++){
     int a, b; cin >> a >> b;
-    cout << (a == b ? 0 : query(a, b)) << endl;
+    cout << ((dist[a][b] == INF) ? -1 : dist[a][b]) << endl;
   }
 
 }
