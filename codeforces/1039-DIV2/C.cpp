4659d4b9ca810b836980cd850607b8d50122a999
IsmailAtchoo <ismail.hatti10@gmail.com>
2025-08-26 13:02:28 +0200

    C solution 1039 div2

---
+++ codeforces/1039-DIV2/C.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+ 
+#define F first
+#define S second
+#define pb push_back
+#define pf push_front
+#define popf pop_front
+#define popb pop_back
+#define MOD 1000000007
+#define MOD2 998244353
+#define vi vector<int>
+#define vii vector<pair<int,int>>
+#define pi pair<int,int>
+ 
+using namespace std;
+#define ll long long
+int const MAX5 = 100000, MAX6 = 1000000;
+ 
+//printf("%.10lf\n",ans);
+//cout<<fixed<<setprecision(20)<<ans<<endl;
+//stoll string -> long long
+ 
+ 
+/*
+*/
+
+void solve(){
+  int n; cin >> n;
+  int b[n]; for(int i = 0; i < n; i++) cin >> b[i];
+
+  // claim : if there is j > i s.t aj >= 2*ai then NO
+  
+  int inf = b[0];
+  for(int i = 1; i < n; i++){
+    if(b[i] >= 2 * inf){
+      cout << "NO" << endl;
+      return;
+    }
+    inf = min(inf, b[i]);
+  }
+
+  cout << "YES" << endl;
+
+}
+ 
+int main(){
+  // in & out files
+  /*
+  freopen("outofplace.in", "r", stdin);
+  freopen("outofplace.out", "w", stdout);
+*/
+  // fast and furious io
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  cout.tie(0);
+
+// testcases
+  int t = 1;  cin >> t;
+  while(t--){
+    solve();
+  }
+  return 0;
+ 
+}
