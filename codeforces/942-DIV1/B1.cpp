065caecdd6c0a9130975ea12380cf0e233c1e1e6
IsmailAtchoo <ismail.hatti10@gmail.com>
2026-02-03 11:06:35 +0100

    warmup 1400 problem

---
+++ codeforces/942-DIV1/B1.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const int N=2000005;
+int tc,n,m; ll ans;
+inline void solve(){
+	cin>>n>>m; ans=0;
+	for(int i=1;i<=m;i++)
+		ans+=(n+i)/(1ll*i*i);
+	cout<<ans-1<<'\n';
+}
+int main(){
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	cin>>tc; while(tc--) solve();
+	return 0;
+}
